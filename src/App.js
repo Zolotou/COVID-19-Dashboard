74e28b50d79f750f2280ba278b1daec3c4c1500a
Valentin <val.fediuk@gmail.com>
2020-12-20 18:57:28 +0100

    feat: start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import classes from './App.module.scss';
+import Map from './components/Map/Map';
+
+function App() {
+  return (
+    <div className={classes.App}>
+        <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Map.module.scss';
+
+const Description = () => {
+
+  return (
+    <div className={classes.Map}>
+        <p>
+            Map2
+        </p>
+    </div>
+  );
+};
+
+export default Description;
