fa9a3e6c7788246b687f783293b4437ce076f44b
ana-dav <opa.super.op@gmail.com>
2020-12-23 00:41:10 +0200

    fix: refactor map to separate component

---
+++ src/components/GlobalSheet/GlobalSheet.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import classes from './GlobalSheet.module.scss'
+
+const Global = () => {
+
+    return (
+        <div className={classes.sheet__wrapper}>
+            <table>
+                <thead>
+
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    );
+};
+  
+export default Global;
